ef3d8848286f3fe2afcc42499fa543da49aa2598
Alejandro Acosta <titanioco@hotmail.com>
2024-05-16 22:27:56 -0500

    Clearing some initial bugs and errors

--- contracts/AaveTransfer.sol
+++ contracts/AaveTransfer.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
+/// @custom:dev-run-script deploy.js
+
 pragma solidity ^0.8.24;
 
 interface ILendingPool {
@@ -19,7 +21,8 @@ interface IAaveCollateralVault {
 
 contract AaveTransfer {
     address public constant AAVE_LENDING_POOL = address(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
-    address public constant AAVE_COLLATERAL_VAULT = address(0x1E0b7E3d45f90c8F41c1631486A3E343F169bE84);
+    address public constant AAVE_COLLATERAL_VAULT = address(0x1E0b7E3D45f90c8f41C1631486A3e343F169bE84);
+
 
     event TransferCompleted(address indexed from, address indexed to);
 
--- test/Lock.ts
+++ test/Lock.ts
@@ -6,9 +6,6 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
-
-
-
 describe("Lock", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
